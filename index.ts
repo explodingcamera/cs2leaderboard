49b68de707738ae318a1f6762e16f91fd9848974
Henry Gressmann <mail@henrygressmann.de>
2023-09-30 18:06:45 +0200

    refactor
    Signed-off-by: Henry Gressmann <mail@henrygressmann.de>

--- index.ts
+++ index.ts
@@ -1,10 +1,8 @@
+import { regions } from './data/meta.json' assert { type: "json" };
+
 const timestamp = new Date().toISOString().replace(/[:.-]/g, '_');
-const day = new Date().toISOString().split('T')[0];
-
-const url = 'https://api.steampowered.com/ICSGOServers_730/GetLeaderboardEntries/v1?format=json&lbname=';
-
-const currentSeason = 'official_leaderboard_premier_season1';
-const regions = ['global', 'europe', 'africa', 'asia', "australia", "china", "northamerica", "southamerica"];
+const url = 'https://api.steampowered.com/ICSGOServers_730/GetLeaderboardEntries/v1?format=json&lbname=official_leaderboard_premier_';
+const currentSeason = 'season1';
 
 const fetchLeaderboard = async (region: string) => {
   const response = await fetch(`${url}${currentSeason}${region === "global" ? "" : "_"+region}`);
@@ -12,11 +10,17 @@ const fetchLeaderboard = async (region: string) => {
   return data;
 }
 
-const promises = regions.map(async (region) => {
+const promises = Object.keys(regions).map(async (region) => {
   const data = await fetchLeaderboard(region);
-  const filename = `./data/${region}_${timestamp}.json`;
-  await Bun.write(filename, JSON.stringify(data));
-  console.log(`Wrote ${filename}`);
+  const filename = `./data/historical/${region}/${timestamp}.json`;
+  const filenameLatest = `./data/latest/${region}.json`;
+  await Promise.all([
+   Bun.write(filename, JSON.stringify(data)),
+   Bun.write(filenameLatest, JSON.stringify(data)),
+  ]);
 });
 
 await Promise.all(promises);
+const datapoints = Bun.file("./data/timestamps.csv").writer();
+await datapoints.write(`${timestamp}\n`);
+await datapoints.flush();
